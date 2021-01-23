146c0db75fbb69bbe852ce02f7b723f19ed021db
francisco-1998 <francmarinc@gmail.com>
2021-01-23 18:41:14 -0500

    Desestructuracion de arreglos

---
+++ src/ejercicios/07-Desesctructuracion-arreglos.ts
@@ -0,0 +1,9 @@
+const dbz:string[] = ["Goku","Vegeta", "Trunks"];
+const [g,v,t] = dbz;
+const [,,x] = dbz;
+const { 1: panaVegeta } = dbz;
+console.log("Personaje 1: ", g);
+console.log("Personaje 2: ", v);
+console.log("Personaje 2x2: ", panaVegeta);
+console.log("Personaje 3: ", t);
+console.log("Personaje 3x2: ", x);
