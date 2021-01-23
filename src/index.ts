89a617530239942489fe4ecbc3602ff5779837e6
francisco-1998 <francmarinc@gmail.com>
2021-01-23 17:56:47 -0500

    Tarea de objetos e interfaces

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,30 @@
     ===== Código de TypeScript =====
 */
 
+interface SuperHero{
+    nombre: string,
+    edad: number,
+    direccion: Direccion
+    mostrarDireccion:()=>string
+}
+interface Direccion{
+    calle: string,
+    pais:string,
+    ciudad:string
+}
 
+const superHeroe:SuperHero = {
+    nombre: "Thor",
+    edad: 30,
+    direccion: {
+        calle: "Main St",
+        pais: "USA",
+        ciudad: "NY"
+    },
+    mostrarDireccion() {
+        return `${this.nombre} - ${this.direccion.ciudad} - ${this.direccion.pais}`
+    }
+}
+
+const dir = superHeroe.mostrarDireccion();
+console.log(dir);
